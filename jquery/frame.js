134f18e7ec2df44ccc779a6c07fb37e514b8d332
demonxian3 <demon@github.com>
2018-08-04 19:31:02 +0800

    frame

--- jquery/frame.js
+++ jquery/frame.js
@@ -1,48 +1,71 @@
-(function(){
+function print(str){ console.log(str); }                                //print默认弹出打印机程序，强制覆盖
 
+(function(){
     var objectName = 'my';
     window[objectName] = {}
 
     function registerFunc(funcname, funcaddr){                          //注册函数
-        if(window[funcname] !== undefined){                               //如果冲突，加入到命名空间对象objectName中
-            console.log(funcname + 'conflict detective!');
+        if(window[funcname] !== undefined){                             //如果冲突，加入到命名空间对象objectName中
+            console.log(funcname + ' conflict detective!');
             window[objectName][funcname] = funcaddr;
-            console.log(window[objectName])
         }else{                                                           //否则直接注册到windows对象里
-            console.log(funcname + 'join window object ok !')
+            console.log(funcname + ' join window object ok !')
             window[funcname] = funcaddr;
         }
     }
 
     registerFunc('write', write);
-    registerFunc('print', print);
+    registerFunc('urlBaseName', urlBaseName);
+    registerFunc('urlPathName', urlPathName);
+    registerFunc('getCookie', getCookie);
+    registerFunc('setCookie', setCookie);
+    registerFunc('delCookie', delCookie);
+    registerFunc('getRequest', getRequest);
     registerFunc('replaceAll', replaceAll);
-    registerFunc('baseUrlName', baseUrlName);
-    registerFunc('onlyPathName', onlyPathName);
-    registerFunc('getRequestData', getRequestData);
-
+    registerFunc('toUrlParams', toUrlParams);
 
     function write(str){
         document.write(str);
     }
-    function print(str){
-        console.log(str);
-    }
-    function getRequestData(){
+    function getRequest(key){
         var GET = new Object();
         var paramArr = location.search.substr(1).split('&');
         for(var i in paramArr){
             var arr = paramArr[i].split('=');
             GET[arr[0]] = arr[1]; 
         }
+        if(key) return GET[key];
         return GET;
     }
-    function baseUrlName(path){
+    function getCookie(key){
+        var cookieObj = new Object();
+        var cookieArr = document.cookie.split('; ');
+        for(var i in cookieArr){
+            var cookiePart = cookieArr[i].split('=');
+            cookieObj[cookiePart[0]] = cookiePart[1];
+        }
+        if(key) return cookieObj[key];
+        return cookieObj;
+    }
+    function delCookie(key){
+        var exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+        var cval=getCookie(key);
+        if(cval!=null)
+            document.cookie= key + "=" + cval + ";expires=" + exp.toGMTString();
+    }
+    function setCookie(key,value,expiredays){
+        var exdate=new Date()
+        exdate.setDate(exdate.getDate()+expiredays)
+        document.cookie=key+ "=" +escape(value)+
+        ((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+    }
+    function urlBaseName(path){
         var name = path || location.href;
         name = name.split('/').pop().split('?')[0];
         return name;
     }
-    function onlyPathName(path){
+    function urlPathName(path){
         var arr = path || location.href;
         arr = arr.split('/');
         arr.pop();
@@ -53,5 +76,38 @@
         var str = arr.join(newStr);
         return str;
     }
-    
+    function ajaxGet(url, callBackFunc){
+        var xmlhttp;
+        if(window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+        else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        xmlhttp.onreadystatechange = function(){
+            if(xmlhttp.readyState == 4 && xmlhttp.status == 200)
+                callBackFunc(responseText);
+        }
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
+    function ajaxPost(url, paramObj, callBackFunc){
+        var xmlhttp;
+        var paramStr = toUrlParams(paramObj);
+        if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+        else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); 
+        xmlhttp.onreadystatechange=function(){
+            if(xmlhttp.readyState==4 && xmlhttp.status==200)
+                callBackFunc(responseText);
+        }
+        xmlhttp.open("POST", url, true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send(paramStr);
+    }
+
+    function toUrlParams(obj){
+        var arr = [];
+        for(var i in obj)
+            arr.push(i + "=" + obj[i]);
+        var params = arr.join("&");
+        return params;
+    }
+    print(toUrlParams({'url':"http","age":33}))
+
 })();
